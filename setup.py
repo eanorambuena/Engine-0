784386dc3a9f6117e0b9066f0e48e792f840ad2c
Emmanuel Norambuena <eanorambuena@uc.cl>
2021-05-12 01:48:46 +0000

    Setup changes.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ptk',
+  packages = ['ptk'], # this must be the same as the name above
+  version = '1.3.8',
+  description = 'Python Tool Kit Engine: a tool kit for Pygame and Pyglet implementations.',
+  author = 'Emmanuel Norambuena',
+  author_email = 'eanorambuena@uc.cl',
+  url = 'https://github.com/eanorambuena/PTK_Engine', # use the URL to the github repo
+  download_url = 'https://github.com/eanorambuena/PTK_Engine/archive/refs/heads/master.zip',
+  keywords = ['graphics', 'videogames'],
+  classifiers = [],
+)
